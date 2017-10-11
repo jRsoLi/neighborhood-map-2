edd3cc1f59f1665d60112f9dbc2cf3f31dc56ac5
Jasper Roll <jasper.roll@whu.edu>
2017-10-12 00:52:39 +0200

    fixes after review

--- js/app.js
+++ js/app.js
@@ -1,101 +1,137 @@
-var Sight = function(title, lat, lng, id) {
-    var self = this;
-    this.title = title;
-    this.lat = lat;
-    this.lng = lng;
-    this.id = id;
+var map;
+var markerArray = ko.observableArray([]);
+
+function initMap() {
+    var mapOptions = {
+        zoom: 14,
+        center: new google.maps.LatLng(53.550022, 9.98556),
+        disableDefaultUI: true
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
 
 
-    this.infowindow = new google.maps.InfoWindow();
+    var Sight = function(title, lat, lng, id) {
+        var self = this;
+        this.title = title;
+        this.lat = lat;
+        this.lng = lng;
+        this.id = id;
 
-    //set default and highlight colors for map markers
-    var defaultIcon = makeMarkerIcon('225FC1');
-    var highlightedIcon = makeMarkerIcon('FC0505');
 
-    //define marker
-    this.marker = new google.maps.Marker({
-        position: new google.maps.LatLng(self.lat, self.lng),
-        map: map,
-        title: self.title,
-        animation: google.maps.Animation.DROP,
-        icon: defaultIcon
-    });
+        this.infowindow = new google.maps.InfoWindow();
 
-    // opens and populates info window for marker
-    this.openInfowindow = function() {
-        for (var i = 0; i < sightsModel.sights.length; i++) {
-            sightsModel.sights[i].infowindow.close();
-        }
-        map.panTo(self.marker.getPosition());
-        self.marker.setAnimation(google.maps.Animation.DROP);
+        //set default and highlight colors for map markers
+        var defaultIcon = makeMarkerIcon('225FC1');
+        var highlightedIcon = makeMarkerIcon('FC0505');
 
-        //get wikipedia description information
-        var description = "description";
-        var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + self.marker.title +
-            '&format=json&callback=wikiCallback';
-
-        $.ajax({
-            url: wikiUrl,
-            dataType: "jsonp",
-            async: false,
-            // jsonp: "callback",
-            success: function(response) {
-
-                description = response[2][0];
-                self.infowindow.setContent('<div>' + '<h3>' + self.marker.title + '</h3>' +
-                    '<p>' + description + '</p>' + '</div>');
-            }
+        //define marker
+        this.marker = new google.maps.Marker({
+            position: new google.maps.LatLng(self.lat, self.lng),
+            map: map,
+            title: self.title,
+            animation: google.maps.Animation.DROP,
+            icon: defaultIcon
         });
-        self.infowindow.open(map, self.marker);
+        markerArray.push(this.marker);
+
+        // opens and populates info window for marker
+        this.openInfowindow = function() {
+            for (var i = 0; i < sightsModel.sights.length; i++) {
+                sightsModel.sights[i].infowindow.close();
+            }
+            map.panTo(self.marker.getPosition());
+            self.marker.setAnimation(google.maps.Animation.DROP);
+
+            //get wikipedia description information
+            var description = "description";
+            var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + self.marker.title +
+                '&format=json&callback=wikiCallback';
+            var wikiRequestTimeout = setTimeout(function() {
+                alert("failed to get wikipedia resources");
+            }, 1000);
+            $.ajax({
+                url: wikiUrl,
+                dataType: "jsonp",
+                success: function(response) {
+
+                    description = response[2][0];
+                    self.infowindow.setContent('<div>' + '<h3>' + self.marker.title + '</h3>' +
+                        '<p>' + description + '</p>' + '</div>');
+                    clearTimeout(wikiRequestTimeout);
+                }
+            });
+            self.infowindow.open(map, self.marker);
+        };
+
+        //creates the marker with specific style
+        function makeMarkerIcon(markerColor) {
+            var markerImage = new google.maps.MarkerImage(
+                'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
+                '|40|_|%E2%80%A2',
+                new google.maps.Size(21, 34),
+                new google.maps.Point(0, 0),
+                new google.maps.Point(10, 34),
+                new google.maps.Size(21, 34));
+            return markerImage;
+        }
+
+        //highlight selected marker
+        this.marker.addListener('mouseover', function() {
+            this.setIcon(highlightedIcon);
+        });
+
+        //de-highlight selected marker
+        this.marker.addListener('mouseout', function() {
+            this.setIcon(defaultIcon);
+        });
+
+        //listener to open info window on click
+        this.addListener = google.maps.event.addListener(self.marker, 'click', (this.openInfowindow));
     };
 
-    //creates the marker with specific style
-    function makeMarkerIcon(markerColor) {
-        var markerImage = new google.maps.MarkerImage(
-            'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
-            '|40|_|%E2%80%A2',
-            new google.maps.Size(21, 34),
-            new google.maps.Point(0, 0),
-            new google.maps.Point(10, 34),
-            new google.maps.Size(21, 34));
-        return markerImage;
-    }
-
-    //highlight selected marker
-    this.marker.addListener('mouseover', function() {
-        this.setIcon(highlightedIcon);
-    });
-
-    //de-highlight selected marker
-    this.marker.addListener('mouseout', function() {
-        this.setIcon(defaultIcon);
-    });
-
-    //listener to open info window on click
-    this.addListener = google.maps.event.addListener(self.marker, 'click', (this.openInfowindow));
-};
-
-//all sights used
-var sightsModel = {
-
-    sights: [
-        new Sight('Speicherstadt', 53.544705, 9.9749689, '1'),
-        new Sight('Elbphilharmonie', 53.5413306, 9.9841274, '2'),
-        new Sight('Fischmarkt', 53.5451634, 9.9532352, '3'),
-        new Sight('Planten un Blomen', 53.5495481, 9.9797395, '4'),
-        new Sight('Aussen-Alster', 53.5674892, 10.0056724, '5')
-    ],
-    query: ko.observable(''),
-};
 
 
-// Search function
-sightsModel.search = ko.dependentObservable(function() {
-    var self = this;
-    var search = this.query().toLowerCase();
-    return ko.utils.arrayFilter(self.sights, function(sight) {
-        return sight.title.toLowerCase().indexOf(search) >= 0;
-    });
-}, sightsModel);
+    //all sights used
+    var sightsModel = {
 
-ko.applyBindings(sightsModel);
+        sights: [
+            new Sight('Speicherstadt', 53.544705, 9.9749689, '1'),
+            new Sight('Elbphilharmonie', 53.5413306, 9.9841274, '2'),
+            new Sight('Fischmarkt', 53.5451634, 9.9532352, '3'),
+            new Sight('Planten un Blomen', 53.5495481, 9.9797395, '4'),
+            new Sight('Aussen-Alster', 53.5674892, 10.0056724, '5')
+        ],
+        query: ko.observable(''),
+    };
+
+
+    // Search function
+    sightsModel.search = ko.computed(function() {
+        var self = this;
+        var search = this.query().toLowerCase();
+        return ko.utils.arrayFilter(self.sights, function(sight) {
+            return sight.title.toLowerCase().indexOf(search) >= 0;
+        });
+    }, sightsModel);
+
+    sightsModel.filteredMarkers = ko.computed(function() {
+        var search = this.query().toLowerCase();
+        return ko.utils.arrayFilter(markerArray(), function(marker) {
+            if (marker.title.toLowerCase().indexOf(search) >= 0) {
+                return marker.setVisible(true);
+            } else {
+                return marker.setVisible(false);
+            }
+        });
+    }, sightsModel);
+
+    ko.applyBindings(sightsModel);
+}
+
+initMap();
+
+//Error handling for Google Maps
+function gError() {
+    document.getElementById("map-canvas").innerHTML = "<h2> Could not find the map you are looking for... </h2>";
+}
